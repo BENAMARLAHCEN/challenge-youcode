161e1ada7c35ffb1c783cbcf5dae8a951d441d8c
lahcen <lahcen.ben3@gmail.com>
2023-09-14 10:58:46 +0100

    chal6

---
+++ challenge6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    float r,T=3.14;
+    printf("Enter the Ray :");
+    scanf("%f",&r);
+    printf("the circumference is :%f",2*T*r);
+    
+    
+    return 0;
+}
