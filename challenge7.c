6ece8b6baf12fe7fab14e280dfc44f4a3ebe5499
lahcen <lahcen.ben3@gmail.com>
2023-09-14 11:08:15 +0100

    chal7

---
+++ challenge7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, inv;
+
+    printf("Entrez un nombre entier à trois chiffres : ");
+    scanf("%d", &n);
+
+    inv = (n % 10) * 100 + ((n / 10) % 10) * 10 + (n / 100);
+
+    printf("L'inverse est : %d", inv);
+
+    return 0;
+}
